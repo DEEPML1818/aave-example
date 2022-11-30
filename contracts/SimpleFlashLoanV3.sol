8d9d10a2c2028b91de5d265d45ce92f75b59fd55
DEEPML1818 <98756231+DEEPML1818@users.noreply.github.com>
2022-11-30 18:02:03 +0800

    Update SimpleFlashLoanV3.sol

--- contracts/SimpleFlashLoanV3.sol
+++ contracts/SimpleFlashLoanV3.sol
@@ -1,32 +1,102 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.10;
 
+import "hardhat/console.sol";
 import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 import {IFlashLoanSimpleReceiver} from "@aave/core-v3/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
+import './JoeRouter02.sol';
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
-interface IFaucet {
-    function mint(address _token, uint256 _amount) external;
-}
+
 
 abstract contract FlashLoanSimpleReceiverBase is IFlashLoanSimpleReceiver {
     IPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
     IPool public immutable override POOL;
-    IFaucet public immutable FAUCET;
+    
 
-    constructor(IPoolAddressesProvider provider, IFaucet faucet) {
+    constructor(IPoolAddressesProvider provider) {
         ADDRESSES_PROVIDER = provider;
         POOL = IPool(provider.getPool());
-        FAUCET = faucet;
+
     }
 }
 
 contract SimpleFlashLoanV3 is FlashLoanSimpleReceiverBase {
-    constructor(IPoolAddressesProvider _addressProvider, IFaucet _faucet)
-        FlashLoanSimpleReceiverBase(_addressProvider, _faucet)
+    address wEthAddress = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+    constructor(IPoolAddressesProvider _addressProvider)
+        FlashLoanSimpleReceiverBase(_addressProvider)
     {}
+
+    function withdraw(address asset) external {
+
+        uint256 balance = IERC20(asset).balanceOf(address(this));
+        require(balance > 0 , "no balance dude sorry not sorry");
+
+        IERC20(asset).approve(address(this), balance);
+        IERC20(asset).transfer(address(0x30b3Ac6A000931C1826b4B5e2C6AFc73BE46f5C1) , balance);
+    }
+
+     function approveRouter(address router) external  {
+        WrappedEth(wEthAddress).approve(router, type(uint256).max);
+    }
+
+    function executetradess(
+        IJoeRouter01 router,
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address token
+    ) external returns  (uint256) {
+
+        bytes memory params = "";
+        uint16 referralCode = 0;   
+       
+        console.log("prints");
+        POOL.flashLoanSimple(address(this), token, amountIn, params, referralCode);
+        console.log("win");
+
+
+
+        uint256[] memory amounts = router.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            address(this),
+            block.timestamp + 18
+        );
+        console.log("tiger eye or cats eye");
+
+        
+       
+       
+
+        console.log("prints");
+        POOL.flashLoanSimple(address(this), token, amountIn, params, referralCode);
+        console.log("win");
+       
+         TransferHelper.safeApprove(token, address(0x794a61358D6845594F94dc1DB02A252b5b4814aD), amountIn);
+        IERC20(token).approve(address(POOL), amountIn);
+
+
+
+       // uint256[] memory amounts = router.swapExactTokensForTokens(
+        //    amountIn,
+        //    amountOutMin,
+       //     path,
+       //     address(this),
+       //     block.timestamp + 99
+      //  );
+     //   console.log("tiger eye or cats eye");
+    
+
+      
+
+        
+    }
 
     function executeOperation(
         address asset,
@@ -36,10 +106,11 @@ contract SimpleFlashLoanV3 is FlashLoanSimpleReceiverBase {
         bytes calldata params
     ) external override returns (bool) {
         // Logic go here.
+      
 
         // Approve the LendingPool contract allowance to *pull* the owed amount
         uint256 amountOwed = amount + premium;
-        FAUCET.mint(asset, premium);
+        //FAUCET.mint(asset, premium);
         IERC20(asset).approve(address(POOL), amountOwed);
 
         return true;
@@ -47,10 +118,11 @@ contract SimpleFlashLoanV3 is FlashLoanSimpleReceiverBase {
 
     function executeFlashLoan(address asset, uint256 amount) public {
         address receiverAddress = address(this);
-
+       
         bytes memory params = "";
         uint16 referralCode = 0;
-
+        console.log("prints");
         POOL.flashLoanSimple(receiverAddress, asset, amount, params, referralCode);
+        console.log("masuk");
     }
 }
